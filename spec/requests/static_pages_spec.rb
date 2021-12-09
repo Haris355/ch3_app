835599072a144e5632a72eda3b8dae770a399820
Haris Bilal <haris.novatoresols@gmail.com>
2021-12-09 15:33:29 +0500

    change (Home,About,Help) staticlly

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,9 +1,25 @@
 require 'spec_helper'
+
 describe "Static pages" do
+
   describe "Home page" do
+
     it "should have the content 'ch3_app'" do
       visit '/static_pages/home'
       page.should have content('ch3_app')
     end
   end
-end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static pages/help'
+      page.should have content('Help')
+    end
+  end
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static pages/about'
+      page.should have content('About Us')
+    end
+  end
+  end
