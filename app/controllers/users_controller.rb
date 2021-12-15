7be8255763aacd9981233ef629c7b4d5607779fb
Haris Bilal <haris.novatoresols@gmail.com>
2021-12-15 11:31:46 +0500

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,37 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def new;
+  def new
+    @user = User.new
   end
 
-  #def create
-  # @user = User.new(params[:user])
-  # if @user.save
-  #   sign_in @user
-  #   flash[:success] = "Welcome!"
-  #   redirect_to @user
-  # else
-  #   render 'new'
-  # end
-  #end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the ch3_app!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
+
+
+
+
+
+
+# def create
+# @user = User.new(params[:user])
+# if @user.save
+#   sign_in @user
+#   flash[:success] = "Welcome!"
+#   redirect_to @user
+# else
+#   render 'new'
+# end
+# end
