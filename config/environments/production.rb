63ef936bd36fddc5cbf87179a32244937baae5b5
Haris Bilal <haris.novatoresols@gmail.com>
2021-12-15 11:36:15 +0500

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,9 @@
-Rails.application.configure do
+Ch3App.Application.configure do
+
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
