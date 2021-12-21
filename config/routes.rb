c2ae1659f8212001c1201be182a3afc13dcb1696
Haris Bilal <haris.novatoresols@gmail.com>
2021-12-21 19:00:36 +0500

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
-
 Ch3App::Application.routes.draw do
   # get 'users/new'
   # match '/help', to: 'static_pages#help'
   # match '/about', to: 'static_pages#about'
   # match '/contact', to: 'static_pages#contact'
 
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   resources :users
-  resources :sessions, only: [:create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   root to: 'static_pages#home'
 
